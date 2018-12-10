62e142687c9debbb948234b3eb4a39440df232a9
XINGRI-0Q5OGRE2\Administrator <1204383390@qq.com>
2018-12-10 10:30:51 +0800

    实现app图片轮播

--- src/screens/ScreenHome/view.js
+++ src/screens/ScreenHome/view.js
@@ -1,25 +1,76 @@
 // 引入依赖
-import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native'
-import pxToDp from '../../config/pxToDp';
+import React, { Component } from 'react'
+import {
+    Text,
+    ScrollView,
+    Image,
+    View,
+    Dimensions
+} from 'react-native'
+import Swiper from 'react-native-swiper'
+const { width,height } = Dimensions.get('window')
+
+const styles = {
+    container: {
+        height:height/5,
+    },
+
+    wrapper: {
+      width: width,
+    },
+
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'transparent'
+    },
+
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB'
+    },
+
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5'
+    },
+
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9'
+    },
+
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+
+    image: {
+        width,
+        height:200
+
+    }
+}
 
 export default self => (
-   <View>
-    <Text style={{ fontSize: pxToDp(36) }}>home</Text>
-
-    <Button title="goSome1" onPress={() => self.navigation.navigate('ScreenSome1')} />
-
-
-    <Button title="goSome2Modal" onPress={() => self.navigation.navigate('ScreenSome2Modal')} />
-
-    <Button
-      title="webView"
-      onPress={() =>
-        self.navigation.navigate('ScreenWebView', {
-          url: 'https://github.com/facebook/react-native',
-          title: '加载中...',
-        })
-      }
-    />
-  </View>
+  <ScrollView style={styles.container}>
+     <Swiper style={styles.wrapper} height={height/5} autoplayTimeout={4} horizontal={true} autoplay={ true }>
+        <View style={styles.slide1}>
+          <Image resizeMode='stretch' style={styles.image} source={require('../../images/swiper1.png')} />
+        </View>
+        <View style={styles.slide2}>
+            <Image resizeMode='stretch' style={styles.image} source={require('../../images/swiper2.png')} />
+        </View>
+        <View style={styles.slide3}>
+          <Image resizeMode='stretch' style={styles.image} source={require('../../images/swiper3.png')} />
+        </View>
+      </Swiper>
+  </ScrollView>
 );
