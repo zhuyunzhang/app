1fa73263786192c924d2487cb0ec9014b3938422
zyz <1204383390@qq.com>
2018-12-19 13:50:13 +0800

    修改充值页面

--- app/Router.js
+++ app/Router.js
@@ -7,7 +7,7 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 import ScreenBottomTab from './Views';;
 import Scanning from './components/home/scanning';
 import RechargeLine from './components/home/rechargeline';
-
+import Dialog from './components/home/rechargeline/dialog';
 /**
  * 自定义 StackNavigator，可以选择 screen 进入方式
  * 默认状态为 card，只需要输入对应页面，比如 ..navigate('ScreenSome1')
@@ -44,7 +44,8 @@ const AppNavigator = StackModalNavigator({
   ScreenBottomTab: ScreenBottomTab,
   //下面几个配置的是测试Navigator不同使用场景用，只需要tab的话，只要ScreenBottomTab: ScreenBottomTab即可
   Scanning:{screen: Scanning },
-  RechargeLine:{screen: RechargeLine }
+  RechargeLine:{screen: RechargeLine },
+  Dialog:{screen: Dialog }
 });
 const App = createAppContainer(AppNavigator)
 export default App
--- app/components/home/rechargeline/index.js
+++ app/components/home/rechargeline/index.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react';
 import { 
     Dimensions, 
-    SafeAreaView,
     SectionList, 
     FlatList,
     Alert,
@@ -11,12 +10,16 @@ import {
     TouchableOpacity, 
     StyleSheet,
     Image,
-    Navigator
+    Modal,
+    TextInput,
+    TouchableHighlight
 } from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as action from '。../../../actions';
+import * as action from '../../../actions';
+import { H3 } from 'native-base';
 const { width, height } = Dimensions.get('window');
+let dialogWidth = width-80;
 
 const numColumns = 3;
 
@@ -24,30 +27,82 @@ const numColumns = 3;
     constructor(props){
       super(props);
       this.navigation = props.navigation;
+      this.state = {
+        modalVisible: false,
+        keys:0,
+        moneys:0
+    };
     }
     componentDidMount() {
         // 隐藏启动页，如果不设置消失时间，在组件加载完启动页自动隐藏
     }
+    setModalVisible(visible,keies,moneies) {
+        this.setState({
+            modalVisible: visible,
+            keys:keies,
+            moneys:moneies
+        });
+    }
+    onClose() {
+        this.setState({modalVisible: false});
+    }
     render() {
         const data = [{
             content: [
-                { key:1,money: '10元', title: '赠送2元'},
-                { key:2,money: '30元', title: '赠送7元'},
-                { key:3,money: '50元', title: '赠送12元'},
-                { key:4,money: '100元', title: '赠送20元'},
-                { key:5,money: '300元', title: '赠送70元'},
-                { key:6,money: '500元', title: '赠送120元'},
+                { key:1,money: 10, title: '赠送2元'},
+                { key:2,money: 30, title: '赠送7元'},
+                { key:3,money: 50, title: '赠送12元'},
+                { key:4,money: 100, title: '赠送20元'},
+                { key:5,money: 70, title: '赠送70元'},
+                { key:6,money: 120, title: '赠送120元'},
             ],
             img: 'content',
         }];
         return (
-            <SafeAreaView style={styles.container}>
+           
+            <View style={styles.container}>
+                <H3 style={{margin:10}}>在线充值</H3>
                 <SectionList
                     sections={[{data}]}
                     renderItem={this._renderSectionItem}
                     keyExtractor={this._keyExtractor}
                     />
-            </SafeAreaView>
+                <View style={{marginTop: 22}}>
+                    <Modal
+                        animationType={"slide"}
+                        transparent={true}
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {this.setModalVisible(false,0,0)}}
+                    >
+                        <TouchableOpacity style={{flex:1}} onPress={this.onClose.bind(this)}>
+                            <View style={stylees.container}>
+                                <View style={stylees.innerContainer}>
+                                    <H3>在线充值</H3>
+                                    <View style={ {flexDirection:'row',height:80,marginTop:10}}>
+                                        <TouchableOpacity onPress={() => this.onPressImage(this.state.keys,this.state.moneys)}>
+                                            <View style={ {flex:1,margin:20 }}>
+                                                <Image source={require('../images/alipay.png')} style={{height: 60,width:60,alignSelf:'center'}}/>
+                                            </View>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity onPress={() => this.onPressImage(this.state.keys,this.state.moneys)}>
+                                            <View style={ {flex:1,margin:20 }}>
+                                                <Image source={require('../images/weipay.png')} style={{height: 60,width:60,alignSelf:'center'}}/>
+                                            </View>
+                                        </TouchableOpacity>          
+                                    </View>
+                                    <View style={stylees.btnContainer}>
+                                        <TouchableHighlight onPress={() => {
+                                            this.setModalVisible(!this.state.modalVisible,0,0);
+                                        }}>
+                                            <Text  style={stylees.hidemodalTxt}>关闭</Text>
+                                        </TouchableHighlight>
+                                    </View>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </Modal>
+                </View>
+            </View>
         )
     }
 
@@ -61,7 +116,9 @@ const numColumns = 3;
             <TouchableOpacity 
                 activeOpacity={0.7}
                 style={styles.item}
-                 onPress={() => this.onPressImage(item.key,item.title)}
+                 onPress={() => {
+                    this.setModalVisible(true,item.key,item.money)
+                }}
             >
               <Text style={styles.itemText1}>{item.money}</Text>
               <Text style={styles.itemText2}>{item.title}</Text>
@@ -69,19 +126,23 @@ const numColumns = 3;
         )
     }
     onPressImage(keys,title) {
-        if(keys===1){
-            const{navigation}=this.props
-            if(navigation){
-                navigation.navigate('Scanning') 
-            }
-        }else if(keys===4){
-            const{navigation}=this.props
-            if(navigation){
-                navigation.navigate('Recharge') 
-            }
-        }else{
-            alert("==>"+title)
-        }
+        alert(keys+"========>>>>>"+title);
+        // if(keys===1){
+        //     const{navigation}=this.props
+        //     if(navigation){
+        //         navigation.navigate('Scanning') 
+        //     }
+        // }else if(keys===4){
+        //     const{navigation}=this.props
+        //     if(navigation){
+        //         navigation.navigate('Recharge') 
+        //     }
+        // }else{
+        //      const{navigation}=this.props
+        //     if(navigation){
+        //         navigation.navigate('Dialog') 
+        //     }
+        // }
         
     }
     _renderSectionItem = ({section}) => {
@@ -129,3 +190,31 @@ const styles = StyleSheet.create({
     }
 
 })
+const stylees = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
+    innerContainer: {
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 20
+
+    },
+    btnContainer:{
+        width:dialogWidth,
+        borderTopWidth:1,
+        borderTopColor:'#777',
+        alignItems:'center'
+    },
+    inputtext:{
+        width:dialogWidth-20,
+        margin:10,
+    },
+    hidemodalTxt: {
+        marginTop:10,
+    },
+});
